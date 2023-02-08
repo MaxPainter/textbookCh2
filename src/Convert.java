b8eebee378b329df5697513dac0014d6059b310d
Max Painter <mpainter1@ggc.edu>
2023-02-07 23:35:31 -0500

    Chapter 2

--- src/Convert.java
+++ src/Convert.java
@@ -1,2 +1,24 @@
+/**
+ * @class: Convert
+ * @author: Max Painter
+ * @written: February 7, 2023
+ * description: Write a program that convert a given total number of seconds into hours, minutes and seconds
+ */
+
+import java.util.Scanner;
+
 public class Convert {
+    public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+        System.out.println("Enter a total number of seconds: ");
+    int number = input.nextInt();
+    int hours = number / 3600;
+    int minutes = (number%3600) / 60;
+    int seconds = number % 60;
+        System.out.print("If the total of number is "+number+", it is "+hours+" hours,"+minutes+" minutes and "+seconds+" seconds.");
+
+
 }
+    }
+
---
+++ src/Paint.java
@@ -0,0 +1,40 @@
+/**
+ * @class: Paint
+ * @author: Max Painter
+ * @written: February 7, 2023
+ * description: the program will calculate the gallons and quarts of paint are needed to paint the room.
+ */
+import java.util.Scanner;
+public class Paint {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Length of the room: ");
+        int l = in.nextInt();
+        System.out.println("Width of the room: ");
+        int w = in.nextInt();
+        System.out.println("Height of the room: ");
+        int h = in.nextInt();
+
+        System.out.println("Number of windows: ");
+        int numWindows = in.nextInt();
+        System.out.println("Number of doors: ");
+        int numDoors = in.nextInt();
+
+        double totalArea = (l * h + w * h) * 2 + l * w - numWindows * 15 - numDoors * 21;
+        System.out.println("Area: " + totalArea);
+        double quarts = 4.0 * totalArea / 350.0;
+
+        System.out.println(quarts + " quarts needed");
+
+        int gallons = 0;
+        while (quarts >= 4) {
+            gallons++;
+            quarts -= 4;
+        }
+
+        System.out.println(gallons + " gallons and " + quarts + " quarts of paint is needed");
+
+
+    }
+}
